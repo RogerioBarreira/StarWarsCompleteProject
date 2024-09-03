f432325ef3a88c2e05f3214108406687dacc67e6
Rogerio Martins Barreira <abacuque37@hotmail.com>
2024-09-03 15:45:11 -0300

    Criação TabBar
    Criada TabBar

---
+++ StarWarsCompleteProject/StarWarsCompleteProject/Sources/Controllers/SWPlanets/View/SWPlanetsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SWPlanetsViewController.swift
+//  StarWarsCompleteProject
+//
+//  Created by Rogerio Martins Barreira on 03/09/24.
+//
+
+import UIKit
+
+class SWPlanetsViewController: UIViewController {
+    
+    lazy var viewSWPlanets: SWPlanetsView = {
+        let view = SWPlanetsView()
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = viewSWPlanets
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
